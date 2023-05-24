0a0da17e1af393ba9afb994488e1038af3f4d4ad
KYIMINHAN <kyiminhan@KYIMINHANs-MacBook-Air.local>
2023-05-24 16:10:35 +0700

    Deposit table added

---
+++ app/Filament/Resources/DepositResource.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DepositResource\Pages;
+use App\Filament\Resources\DepositResource\RelationManagers;
+use App\Models\Deposit;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DepositResource extends Resource
+{
+    protected static ?string $model = Deposit::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDeposits::route('/'),
+            'create' => Pages\CreateDeposit::route('/create'),
+            'edit' => Pages\EditDeposit::route('/{record}/edit'),
+        ];
+    }    
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -24,6 +24,7 @@ class User extends Authenticatable implements BannableContract
         'name',
         'email',
         'password',
+        'money',
     ];
 
     /**
